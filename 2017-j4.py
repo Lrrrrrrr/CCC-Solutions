3e58d953ccf231217e1713305f9a5ec331c5533e
MohammedAl-Rasheed <mohammed.satar.alrasheed@gmail.com>
2021-01-26 16:52:38 -0500

    First commit

--- 2017-j4.py
+++ 2017-j4.py
@@ -1,31 +1,48 @@
-input = int(input())
-hour = 12
-minute = 0
-counter = 0 
+duration = int(input())
+counter = 0
 
-for i in range (input):
-  minute = int(minute)
-  hour = int(hour)
-  minute += 1 
-  if minute >= 60:
-    minute -= 60
-    if hour + 1 < 13:
-      hour += 1
-    else:
-      hour = (hour + 1) % 12
-  if minute < 10:
-    minute = "0" + str(minute)
-  else:
-    minute = minute 
-  minute = str(minute)
-  hour = str(hour)
-  time = hour + minute
-  lengthtime = len(time)
-  if lengthtime == 3:
-    if (int(time[2]) - int(time[1])) == (int(time[1]) - int(time[0])):
-      counter += 1 
-  else:
-    if ((int(time[3]) - int(time[2])) == (int(time[2]) - int(time[1]))) and ((int(time[2]) - int(time[1])) == (int(time[1]) - int(time[0]))):
-      counter += 1 
+half_days = duration // 720 
+other_minutes = duration % 720 
+
+for t in range(other_minutes + 1):
+  #print("t={0:3d}".format(t))
+  hour = t // 60
+  if (hour == 0):
+    hour = 12
+  minute = t % 60
   
-print(counter)
+  # 4 digits
+  hour_tens = hour // 10
+  hour_ones = hour % 10
+
+  minute_tens = minute // 10 
+  minute_ones = minute % 10
+
+  #print( "{0:1d} {1:1d} {2:1d} {3:1d}".format(hour_tens, hour_ones, minute_tens, minute_ones))
+  
+  if hour_tens == 1:
+    if (hour_tens - hour_ones) == (hour_ones - minute_tens) and (hour_ones - minute_tens) == (minute_tens - minute_ones):
+      counter = counter + 1
+  else:
+    if (hour_ones - minute_tens) == (minute_tens - minute_ones) :
+      counter = counter + 1
+
+
+total = half_days * 31 + counter 
+print (total)
+
+# sequences per day
+#seq = 31
+# how many days 
+#days = duration / 720
+# get the days until last day without the last day
+#daysint = int(days)
+# get the decimal place remainder 
+#days = days - daysint
+# convert the remainder to percentage
+#percentage = "{:.0%}".format(days)
+# get the amount of minutes that percentage values
+# 
+
+#counter = daysint * seq
+
